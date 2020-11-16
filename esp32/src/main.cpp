1d70c0c24721c1223435463d0d9af0f9121f74aa
Praveena1989 <pravisiva757@gmail.com>
2020-11-16 14:05:55 +0100

    deleted ntp.cpp and improved  main.cpp

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,10 +1,17 @@
 #include <bsp.h>
 #include <time.h>
-#include <common.h>
 #include <bsp_time.h>
+#include <common.h>
 #include <i2c_driver.h>
 #include <led_driver.h>
 #include <wifi_driver.h>
+
+// should remove these macros
+#define DATE_TIME_LENGTH 19
+#define TIME_OFFSET (1 * 60 * 60)
+#define TIME_DAYLIGHT (1 * 60 * 60)
+#define NTP_ADDRESS "pool.ntp.org"
+/*********************************/
 
 void setup()
 {
@@ -17,22 +24,44 @@ void setup()
         while (true)
         {
             // blink the led
+            led_driver_turn_on();
             bsp_delay(500);
+            led_driver_turn_off();
         }
     }
 
     wifi_driver_init();
     wifi_driver_connect();
 
-    uint8_t status = NTP_ERROR;
-    while (status != OKAY)
+    if (wifi_driver_status())
     {
-        //NTP init and get the time
-        configTime(TIME_OFFSET, TIME_DAYLIGHT, NTP_ADDRESS);
+        uint8_t status = NTP_ERROR;
+        while (status != OKAY)
+        {
+            struct tm timeinfo;
+            uint8_t datetime[DATE_TIME_LENGTH + 1] = {};
 
-        // get time
-        // if time is not okay then send ntp error
-        // else send date and time to teensy and set status to okay
+            //NTP init and get the time
+            configTime(TIME_OFFSET, TIME_DAYLIGHT, NTP_ADDRESS);
+
+            // get time
+            if (getLocalTime(&timeinfo))
+            {
+                strftime((char *)datetime, DATE_TIME_LENGTH, "%Y-%m-%d %H:%M:%S", &timeinfo);
+                // write date time to teensy and set the status to okay
+                i2c_driver_write(datetime, sizeof(datetime));
+                status = OKAY;
+            }
+            else
+            {
+                // if time is not okay, send ntp error message to the teensy
+                i2c_driver_write((uint8_t *)NTP_ERROR, sizeof(NTP_ERROR));
+            }
+        }
+    }
+    else
+    { // send wifi disconnected message to the teensy
+        i2c_driver_write(uint8_t *)WIFI_DISCONNECTED, sizeof(WIFI_DISCONNECTED));
     }
 }
 
