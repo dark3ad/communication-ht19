ef73f3d19b9f15f0a69538783eae685712066486
Faroch Mehri <faroch.mehri@gmail.com>
2020-11-28 19:54:49 +0100

    Fixed main.cpp

--- teensy/src/main.cpp
+++ teensy/src/main.cpp
@@ -1,7 +1,16 @@
-//#include <bsp.h>
+#include <bsp.h>
+#include <canbus.h>
+#include <common.h>
+#include <bsp_time.h>
 
 void setup()
 {
+#ifdef DEVELOPMENT
+    bsp_serial_begin();
+#endif
+
+    bsp_delay(3000);
+    PRINTF("%d\n", get_humidity());
 }
 
 void loop()
